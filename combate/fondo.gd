73acd87b1f7b6c9bb399dabafb270eddb11dd275
Lorena <rosales.cujaz18@zaragoza.salesianos.edu>
2019-05-06 12:49:33 +0200

    Signed-off-by: Lorena <rosales.cujaz18@zaragoza.salesianos.edu>

---
+++ combate/fondo.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite
+var contadorDelta
+
+func _ready():
+	contadorDelta = 0
+
+func _process(delta):
+	cambiarFotogramaTrasXTiempo(0.15, delta)
+	print(contadorDelta)
+
+func cambiarFotograma():
+	frame += 1
+	if(frame >= 3):
+		frame = 0
+
+func cambiarFotogramaTrasXTiempo(cadaCuanto, delta):
+	contadorDelta += delta
+	if(contadorDelta >= cadaCuanto):
+		cambiarFotograma()
+		contadorDelta = 0
+	
